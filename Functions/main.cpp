8817d42b7d88f8f4f1f8e66ef3504f94d4358cd5
Soro_pz45@itstepru.onmicrosoft.com <Soro_pz45@itstepru.onmicrosoft.com>
2021-02-28 15:13:56 +0300

    Shift + Function

--- Functions/main.cpp
+++ Functions/main.cpp
@@ -1,5 +1,40 @@
 #include <iostream>
 using namespace std;
+#define tab "\t"
+
+void FillRand(int arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+
+		arr[i] = rand();
+		// функци€ rand() возвращает псевдослучайное число в диапазоне от 0 до 32 767
+	}
+}
+
+void Print(int arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << tab;
+	}
+	cout << endl;
+
+}
+
+void Shift(int arr[], const int n, const int shift)
+{
+	for (int i = 0; i < shift; i++)
+	{
+		int buffer = arr[0];
+		for (int j = 0; j < n; j++)
+		{
+			arr[j] = arr[j + 1];
+		}
+		arr[n - 1] = buffer;
+
+	}
+}
 
 void main()
 
@@ -8,4 +43,34 @@ void main()
 
 	cout << "Hello Functions" << endl;
 
+	const int n = 5;
+	int arr[n]; 
+	FillRand(arr, n);
+	cout << endl;
+	Print(arr, n);
+	cout << endl; 
+
+	int shift;
+	cout << "¬ведите количество сдвигов: "; cin >> shift; cout << endl;
+
+	Shift(arr, n, shift);
+	cout << endl;
+	Print(arr, n);
+
+	////////////////////////////////////////////////////////////////////////
+	cout << "-----------------------------------------------------" << endl;
+
+	const int m = 8;
+	int brr[m];
+
+	FillRand(brr, m);
+	cout << endl;
+	Print(brr, m);
+	cout << endl;
+
+	cout << "¬ведите количество сдвигов: "; cin >> shift; cout << endl;
+
+	Shift(brr, m, shift);
+	cout << endl;
+	Print(brr, m);
 }
